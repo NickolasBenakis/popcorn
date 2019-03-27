5a6912a400c1840e3e764089f276bee999094fdb
NickolasBenakis <nickolasbenele@gmail.com>
2019-03-27 22:05:07 +0200

    Add navBar styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './navBar/navBar';
-
+import logo from './logo.svg';
 export default class App extends Component {
   render() {
-    return (<div className="App" >
-      <NavBar />
-    </div>
+    return (
+      <div className="App" >
+        <NavBar />
+        <div className="Main">
+          Hello Content'
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
     );
   }
 }
