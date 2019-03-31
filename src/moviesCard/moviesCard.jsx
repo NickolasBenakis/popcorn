400b81d37e893ed8f0e1eaeb6f898429a75e94df
NickolasBenakis <nickolasbenele@gmail.com>
2019-03-31 11:59:20 +0300

    Fix carousel position

--- src/moviesCard/moviesCard.jsx
+++ src/moviesCard/moviesCard.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-
+import './moviesCard.scss';
 
 export default class MoviesCard extends Component {
     render() {
         return (
             <div style={{ padding: "20px" }}>
-                <Card style={{ width: '18rem' }}>
+                <Card className="test" style={{ width: '18rem', margin: "20px" }}>
                     <Card.Img className="card-img" variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/CaptainMarvel18.jpg/175px-CaptainMarvel18.jpg" />
                     <Card.Body>
                         <Card.Title>Card Title</Card.Title>
