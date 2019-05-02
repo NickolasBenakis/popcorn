6929065bf2e5fd866ab3c35751b7eb04a109e5a8
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-02 23:40:49 +0300

    Remove comments from moviesList

--- src/app/components/moviesCard/moviesList.js
+++ src/app/components/moviesCard/moviesList.js
@@ -3,10 +3,6 @@ import React from 'react';
 import MoviesCard from './moviesCard';
 export default function MoviesList({ mockMovies }) {
 
-    // const cardsArray = robots.map(user => {
-    //   return <Card key={user.id} id={user.id} name={user.name} email={user.email} />
-    // })
-
     return (
         <div style={{ display: "flex", flexFlow: "row wrap", background: "whitesmoke" }}>
             {
