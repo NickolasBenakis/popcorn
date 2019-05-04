9cd4c6809cd3a9369be9394437a1e4cc775e7ded
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-04 13:24:21 +0300

    Add card styles  && media queries

--- src/app/components/moviesCard/moviesCard.jsx
+++ src/app/components/moviesCard/moviesCard.jsx
@@ -6,18 +6,18 @@ import './moviesCard.scss';
 export default class MoviesCard extends Component {
     render() {
         return (
-            <div style={{ padding: "20px" }}>
-                <Card className="test">
-                    <Card.Img className="card-img" variant="top" src={`https://robohash.org/${this.props.id}?200x200`} alt='movie' />
-                    <Card.Body>
-                        <Card.Title style={{ padding: "10px", fontSize: "11px" }}>{this.props.title}</Card.Title>
-                        <Card.Text>
-                            {this.props.vote_average}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
+            // <div style={{ padding: "20px" }}>
+            <Card className="CardContainer">
+                <Card.Img className="card-img" variant="top" src={`https://robohash.org/${this.props.id}?200x200`} alt='movie' />
+                <Card.Body>
+                    <Card.Title style={{ padding: "10px", fontSize: "11px" }}>{this.props.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.vote_average}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            // </div>
         )
     }
 }
--- src/app/components/moviesCard/moviesList.js
+++ src/app/components/moviesCard/moviesList.js
@@ -4,7 +4,7 @@ import MoviesCard from './moviesCard';
 export default function MoviesList({ mockMovies }) {
 
     return (
-        <div style={{ display: "flex", flexFlow: "row wrap", background: "whitesmoke" }}>
+        <div className="moviesList">
             {
                 mockMovies.map(movie => {
                     return <MoviesCard key={movie.id} id={movie.id} title={movie.title} vote_average={movie.vote_average} />
