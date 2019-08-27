9c294a1d2b84490ee4f2a0a7e1f9733b57cd0631
nickolasBenakis <nickolasbenele@gmail.com>
2019-08-27 23:18:39 +0300

    Add hooks state, lifecycle for Theater components, replaced classfull comps with functions

--- src/app/components/theaters/theaterCard.jsx
+++ src/app/components/theaters/theaterCard.jsx
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './theaterCard.scss';
 import '../moviesCard/moviesCard.scss';
-import mars from '../../../assets/mars.png';
-import jupiter from '../../../assets/jupiter.png';
-import earth from '../../../assets/earth.png';
 import { Link } from 'react-router-dom';
 
-class TheaterCard extends Component {
-    render() {
-        return (
-            <Card className="CardContainer">
-                <Card.Img
-                    className="card-img"
-                    variant="top"
-                    src={`https://res.cloudinary.com/nickolasben/image/upload/w_450,c_scale/popcorn/${this.props.name.toLowerCase()}.png`}
-                    alt="theater"
-                />
-                <Card.Body>
-                    <Card.Title style={{ padding: '10px', fontSize: '11px' }}>
-                        {this.props.name}
-                    </Card.Title>
-                    <Link to="/movie">
-                        <Button variant="primary">
-                            Theater no.{this.props.id}
-                        </Button>
-                    </Link>
-                </Card.Body>
-            </Card>
-        );
-    }
+function TheaterCard({ name, id }) {
+    return (
+        <Card className="CardContainer">
+            <Card.Img
+                className="card-img"
+                variant="top"
+                src={`https://res.cloudinary.com/nickolasben/image/upload/w_450,c_scale/popcorn/${name.toLowerCase()}.png`}
+                alt="theater"
+            />
+            <Card.Body>
+                <Card.Title style={{ padding: '10px', fontSize: '11px' }}>
+                    {name}
+                </Card.Title>
+                <Link to="/movie">
+                    <Button variant="primary">Theater no.{id}</Button>
+                </Link>
+            </Card.Body>
+        </Card>
+    );
 }
+
 export default TheaterCard;
--- src/app/components/theaters/theatersTab.jsx
+++ src/app/components/theaters/theatersTab.jsx
@@ -1,36 +1,32 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import TheatersList from './theatersList.js';
 import fetchAuditoriums from '../../../api/fetchAuditoriums';
 import '../moviesTab/moviesTab.scss';
 
-class TheatersTab extends Component {
-    constructor() {
-        super();
-        this.state = {
-            auditoriums: [],
-        };
-    }
-    componentWillMount() {}
-    componentDidMount() {
-        this.getAuditoriums();
-    }
-    getAuditoriums = async () => {
+function TheatersTab() {
+    const [auditoriums, setAuditoriums] = useState([]);
+
+    // like componentDidMount
+    useEffect(() => {
+        getAuditoriums();
+    }, []);
+
+    const getAuditoriums = async () => {
         const res = await fetchAuditoriums();
-        this.setState({ auditoriums: res });
+        setAuditoriums(res);
     };
-    render() {
-        return (
-            <Fragment>
-                <div id="MovieCards">
-                    {this.state.auditoriums.length ? (
-                        <TheatersList auditoriums={this.state.auditoriums} />
-                    ) : (
-                        <div className="loading-bar"></div>
-                    )}
-                </div>
-            </Fragment>
-        );
-    }
+
+    return (
+        <Fragment>
+            <div id="MovieCards">
+                {auditoriums.length ? (
+                    <TheatersList auditoriums={auditoriums} />
+                ) : (
+                    <div className="loading-bar"></div>
+                )}
+            </div>
+        </Fragment>
+    );
 }
 
 export default TheatersTab;
