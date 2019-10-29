523aa832eb4dc2ecdb238d68d4f1c2b53d058474
nickolasBenakis <nickolasbenele@gmail.com>
2019-10-29 19:32:04 +0200

    fix typo in deleteUser

--- src/api/user/deleteUser.js
+++ src/api/user/deleteUser.js
@@ -1,5 +1,5 @@
 export default id => {
-    const url = `http://localhost:5000/popCornCinemaApi/Users/DeleteUserAsync/${id}`;
+    const url = `http://localhost:5000/popCornCinemaApi/Users/DeleteUser/${id}`;
     return fetch(url, {
         method: 'DELETE',
         headers: {
